9cec0ef2b71cc0e30ef15d3675c25de54da34aa0
KC21077 <ychen32@uw.edu>
2023-04-10 17:37:06 -0700

    hw2

--- question-2.sql
+++ question-2.sql
@@ -1,5 +1,5 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
-SELECT employees.FirstName, employees.LastName, invoices.InvoiceId
+SELECT employees.FirstName  || " " || employees.LastName AS " Agent_Name", invoices.InvoiceId, invoices.CustomerId, invoices.InvoiceDate, invoices.BillingAddress, invoices.BillingCity, invoices.BillingState, invoices.BillingCountry, invoices.BillingPostalCode, invoices.Total
 FROM invoices
 INNER JOIN customers
 ON invoices.CustomerId=customers.CustomerId
